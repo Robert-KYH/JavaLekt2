96e32160323db784394d25225c61749bcbced2e4
Robert <robert.leffmann@student.kyh.se>
2021-09-16 13:31:51 +0200

    Förkorta koden

--- src/kyh/lektion2/Main.java
+++ src/kyh/lektion2/Main.java
@@ -3,7 +3,6 @@ package kyh.lektion2;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.print("lektion 2 -- ");
-    System.out.print("en uppdatering");
+    System.out.println("lektion 2 -- en uppdatering");
   }
 }
