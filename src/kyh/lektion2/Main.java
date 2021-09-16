59d58bae8fc5b2ca3b830300f9296a0d1286092e
Robert <robert.leffmann@student.kyh.se>
2021-09-16 14:24:49 +0200

    Lägg till nerräkning

--- src/kyh/lektion2/Main.java
+++ src/kyh/lektion2/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
   public static void main(String[] args) {
     System.out.println("lektion 2 -- en uppdatering");
+    räkna(10);
+  }
+
+  private static void räkna(int n) {
+    for (; n > 0; --n)  System.out.print(n);
+    System.out.println("---");
   }
 }
